37254ba0a1ec97489ae590a9342dac5175201c3b
broux <bapt.roux@gmail.com>
2018-11-20 22:36:56 +0100

    Upadte path and init script. WARN: useless link generated with init ...

--- init.sh
+++ init.sh
@@ -4,39 +4,51 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # Export .dotfiles as simlink
 ################################################################################
 
-### Bash config
-bash_config=bash/bashrc
-bash_trgt_path=~/.bashrc
+### Bash config {{{
+bashrc_config=bash/bashrc
+bashrc_trgt_path=~/.bashrc
 ln -i -s $SCRIPTPATH/$bash_config $bash_trgt_path
+inputrc_config=bash/inputrc
+inputrc_trgt_path=~/.inputrc
+ln -i -s $SCRIPTPATH/$inputrc_config $inputrc_trgt_path
+### }}}
 
-### Zsh config
-zsh_config=zsh/zshrc
-zsh_trgt_path=~/.zshrc
-ln -i -s $SCRIPTPATH/$zsh_config $zsh_trgt_path
+### Zsh config {{{
+# zsh_config=zsh/zshrc
+# zsh_trgt_path=~/.zshrc
+# ln -i -s $SCRIPTPATH/$zsh_config $zsh_trgt_path
+### }}}
 
-### Vim config
-./cnfVim/cnfLoad.sh
-
-### Awesome config
+### Submodules configuration {{{
+# Vim and tmux config
+./cnfNvimux/cnfLoad.sh
+# Awesome config
 ./cnfAwesome/cnfLoad.sh
+### }}}
 
-### I3 config
+### I3 config {{{
 i3_config=i3
 i3_trgt_path=~/.config/i3
 ln -i -s $SCRIPTPATH/$i3_config $i3_trgt_path
+### }}}
 
-### Xresources config
+### Xresources config {{{
 urxvt_config=urxvt/Xresources
 urxvt_trgt_path=~/.Xresources
 ln -i -s $SCRIPTPATH/$urxvt_config $urxvt_trgt_path
+### }}}
 
-### Zathura config
+### Zathura config {{{
 zathura_config=pdf/zathurarc
 zathura_trgt_path=~/.config/zathura/zathurarc
 ln -i -s $SCRIPTPATH/$zathura_config $zathura_trgt_path
+### }}}
 
-### luakit config
+### luakit config {{{
 mkdir -p ~/.config/luakit
 luakit_config=luakit/userconf.lua
 luakit_trgt_path=~/.config/luakit/userconf.lua
 ln -i -s $SCRIPTPATH/$luakit_config $luakit_trgt_path
+### }}}
+
+# vim: fdm=marker ts=2 sts=2 sw=2 fdl=0
