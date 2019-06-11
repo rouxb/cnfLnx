2f4e10aefeada5acbfc8b7dce454ee4ea4b14f42
Baptiste Roux <baptiste.roux@icalps.com>
2019-06-11 17:19:40 +0200

    Add host relative configuration

---
+++ bash/host/server2.icalps.com.bash
@@ -0,0 +1,8 @@
+# Dedicated bash alias for server2
+FeWspce=DigitalFE/Workspaces
+# Mopic project {{{
+prj_mopic=/prj/mopic/
+alias w_mopic='cd ${prj_mopic}/${FeWspce}/${USER}'
+alias p_mopic='cd ${prj_mopic}'
+alias i_mopic='source ${prj_mopic}/${FeWspce}/${USER}/Setup/setup.sh'
+# }}}
